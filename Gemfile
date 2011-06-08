1aa4376133fac97cb82f169058152e7b31b96415
Chris Boertien <chris.boertien@gmail.com>
2011-06-08 15:51:44 -0700

    Initial commit, rails 3.1.rc2 app
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0.rc2'
+gem 'sqlite3'
+
+# Asset template engines
+gem 'sass-rails', "~> 3.1.0.rc"
+gem 'coffee-script'
+gem 'uglifier'
+
+gem 'jquery-rails'
+
+group :test do
+  gem 'rspec-rails', '2.6.1.beta1'
+  gem 'spork', '0.9.0.rc8'
+  gem 'factory_girl_rails', :require => false
+  gem 'shoulda-matchers'
+  gem 'faker'
+  gem 'cover_me'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rmc::Application.config.secret_token = 'f498e52aa201d6c83750a5a72ee7a45145c1a31c5189112cbcfa3f948cec9b78de3bee422a53c864c0a5b0756becfe85eb8e23b37b836b0c35a04203e2811dac'
